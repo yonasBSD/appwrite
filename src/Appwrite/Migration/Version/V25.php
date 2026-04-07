9ea0b2bc2ff1579a135d1821604bf9976fa8391a
Matej Bačo <matejbaco2000@gmail.com>
2026-04-07 15:56:54 +0200

    formatting fix

--- src/Appwrite/Migration/Version/V25.php
+++ src/Appwrite/Migration/Version/V25.php
@@ -3,14 +3,7 @@
 namespace Appwrite\Migration\Version;
 
 use Appwrite\Migration\Migration;
-use Exception;
 use Throwable;
-use Utopia\Console;
-use Utopia\Database\Database;
-use Utopia\Database\Document;
-use Utopia\Database\Exception\Conflict;
-use Utopia\Database\Exception\Structure;
-use Utopia\Database\Exception\Timeout;
 
 class V25 extends Migration
 {
