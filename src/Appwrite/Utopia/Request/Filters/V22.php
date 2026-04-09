d3c73fbb49e0c86ad59f392ad40937d916a6f65a
Matej Bačo <matejbaco2000@gmail.com>
2026-04-09 15:34:50 +0200

    Add endpoints to control protocols and services

--- src/Appwrite/Utopia/Request/Filters/V22.php
+++ src/Appwrite/Utopia/Request/Filters/V22.php
@@ -7,9 +7,45 @@ use Appwrite\Utopia\Request\Filter;
 class V22 extends Filter
 {
     // Convert 1.9.0 params to 1.9.1
+    protected function parseUpdateProtocolStatus(array $content): array
+    {
+        if (isset($content['api'])) {
+            $content['protocolId'] = $content['api'];
+            unset($content['api']);
+        }
+
+        if (isset($content['status'])) {
+            $content['enabled'] = $content['status'];
+            unset($content['status']);
+        }
+
+        return $content;
+    }
+
+    protected function parseUpdateServiceStatus(array $content): array
+    {
+        if (isset($content['service'])) {
+            $content['serviceId'] = $content['service'];
+            unset($content['service']);
+        }
+
+        if (isset($content['status'])) {
+            $content['enabled'] = $content['status'];
+            unset($content['status']);
+        }
+
+        return $content;
+    }
+
     public function parse(array $content, string $model): array
     {
         switch ($model) {
+            case 'project.updateServiceStatus':
+                $content = $this->parseUpdateServiceStatus($content);
+                break;
+            case 'project.updateProtocolStatus':
+                $content = $this->parseUpdateProtocolStatus($content);
+                break;
         }
         return $content;
     }
