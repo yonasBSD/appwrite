5beeca5a992b590c616d700f076cbf28c75c9e62
Matej Bačo <matejbaco2000@gmail.com>
2026-04-23 15:57:09 +0200

    Placeholder test

--- tests/e2e/Services/Project/ProjectCustomServerTest.php
+++ tests/e2e/Services/Project/ProjectCustomServerTest.php
@@ -11,4 +11,11 @@ class ProjectCustomServerTest extends Scope
     use ProjectBase;
     use ProjectCustom;
     use SideServer;
+
+    // Just a blank test so we dont have warning about empty test class
+    // You can remove this after adding some custom server tests, or some project base tests
+    public function testProjectServerLogic(): void
+    {
+        $this->assertTrue(true);
+    }
 }
