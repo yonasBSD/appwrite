8f68a59a797e9297b9229bc19c318502b6ef7893
Matej Bačo <matejbacocom@gmail.com>
2026-05-03 19:51:56 +0200

    Apply suggestions from code review
    Co-authored-by: Matej Bačo <matejbaco2000@gmail.com>

--- app/config/scopes/project.php
+++ app/config/scopes/project.php
@@ -214,15 +214,16 @@ return [
     'executions.write' => [
         'description' => 'Access to create function executions',
         'category' => 'Functions',
-        'deprecated' => true,
     ],
     'execution.read' => [
         'description' => 'Access to read function executions. This scope is deprecated for consistency purposes, and replaced by `executions.read`.',
         'category' => 'Functions',
+        'deprecated' => true,
     ],
     'execution.write' => [
         'description' => 'Access to create function executions. This scope is deprecated for consistency purposes, and replaced by `executions.write`.',
         'category' => 'Functions',
+        'deprecated' => true,
     ],
 
     // Sites
